2baf5c9288e49275ae448cdaace70fbcc3a4b007
Angel D. Munoz <scythes.lol@gmail.com>
2023-08-05 21:06:03 -0600

    add grid extensions

--- src/Library/Extensions.fs
+++ src/Library/Extensions.fs
@@ -1,2 +1,21 @@
 [<AutoOpen>]
-module Extensions
+module Library.Extensions
+
+open System.Runtime.CompilerServices
+open Avalonia.Controls
+open Avalonia.Controls.Presenters
+open Avalonia
+
+
+[<Extension>]
+type GridExtensions =
+
+  [<Extension>]
+  static member inline rowDefinitions(grid: Grid, rows: string) =
+    grid.RowDefinitions <- RowDefinitions.Parse rows
+    grid
+
+  [<Extension>]
+  static member inline columnDefinitions(grid: Grid, columns: string) =
+    grid.ColumnDefinitions <- ColumnDefinitions.Parse columns
+    grid
